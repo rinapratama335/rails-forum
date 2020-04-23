70bcf302f3635f08015218b41f755588a2781d4f
irwantowibowo1992 <irwantowibowo1992@hotmail.com>
2020-04-24 01:17:39 +0700

    redirect ketika tidak ada akses

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
     include Pundit
+
+    rescue_from Pundit::NotAuthorizedError, with: :pundit_error
+
+    private
+    def pundit_error
+        flash[:notice] = "Anda tidak memiliki hak akses ini"
+        redirect_to root_path
+    end
 end
